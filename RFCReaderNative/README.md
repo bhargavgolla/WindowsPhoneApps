c5cf89e551193b0937752e553b20c0c5ebcf3aea
Bhargav Golla <bhargav.golla@gmail.com>
2012-09-13 18:01:40 +0530

    Committing native version of RFCReader

---
+++ RFCReaderNative/README.md
@@ -0,0 +1,10 @@
+RFC Reader
+==========
+
+This is the Native version of RFC Reader app present in my Phonegap apps repo.
+
+Future work
+-----------
+
+Create an app bar that lets people share this app on social networking sites.
+Make the search history permanent and create hyperlinks to history pages.
---
+++ RFCReaderNative/RFCReaderNative/PanoramaPage1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace RFCReaderNative
+{
+    public partial class PanoramaPage1 : PhoneApplicationPage
+    {
+        public PanoramaPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string site;
+            site = textBox1.Text;
+            if (site.Length != 4)
+                MessageBox.Show("RFC Doc ID has to be 4 digit. Please enter the correct ID");
+            else
+            {
+                site = "http://docs.google.com/viewer?url=http://tools.ietf.org/pdf/rfc" + site + ".pdf";
+                webBrowser1.Navigate(new Uri(site, UriKind.Absolute));
+            }
+        }
+    }
+}
